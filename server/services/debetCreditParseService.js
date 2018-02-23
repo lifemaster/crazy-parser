c2301ade3dd14245d1842ec27b1de78e30f1dbca
lifemaster <kostya.shyika@gmail.com>
2018-02-23 17:13:03 +0200

    Replace stream reading file fo async reading in debet-credit-parser service

--- server/services/debetCreditParseService.js
+++ server/services/debetCreditParseService.js
@@ -5,34 +5,15 @@ const domain = require('domain');
 const debetCreditParser = require('../parsers/debetCreditParser');
 
 module.exports = filePath => {
-  const stream = fs.ReadStream(filePath);
-  let dataArr;
-
   return new Promise((resolve, reject) => {
-    const parserDomain = domain.create();
-
-    parserDomain.on('error', err => reject(err));
-
-    stream.on('open', () => console.log('File has been opened'));
-    stream.on('close', () => console.log('File has been closed'));
-    stream.on('error', err => {
-      console.log('An error has occurred: ', err);
-      reject(err);
-    });
-
-    stream.on('readable', () => {
-      const data = stream.read();
-
-      if (data) {
-        dataArr = iconv.decode(data, 'CP866').split('\r\n');
-      }
-    });
-
-    stream.on('end', () => {
-      console.log('File has been read successfully');
-      parserDomain.run(() => {
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        const decodedData = iconv.decode(data, 'CP866');
+        const dataArr = decodedData.toString().split('\r\n');
         resolve(debetCreditParser(dataArr));
-      });
+      }
     });
   });
 }
